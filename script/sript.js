f54b55c3c235ab51db2ad56d43a853fc02573a2d
Tymofii <timajaf2005@gmail.com>
2025-04-27 18:50:05 +0200

    new-inputs

--- script/sript.js
+++ script/sript.js
@@ -2,8 +2,12 @@ const buttonOpenReg = document.querySelector(".registration")
 const modalWind = document.querySelector(".modal__login")
 const modalRegWind = document.querySelector(".modal__register")
 const buttonOpenLog = document.querySelector(".login")
-
+const headerClose = document.querySelector("wrap header__wrap")
 const crossCloseReg = document.querySelector(".modal__button--img")
+let isModalLogOpen = 0;
+let isModalRegOpen = 0;
+
+
 modalWind.style.cssText = `
 display: flex;
 visibility: hidden;
@@ -29,23 +33,32 @@ const closeModal = event => {
     if (target === modalWind || target === crossCloseReg) {
         modalWind.style.visibility = 'hidden';
         modalWind.style.opacity = 0;
+        isModalLogOpen = 0
     } else if (target === modalRegWind) {
         modalRegWind.style.visibility = 'hidden'
         modalRegWind.style.opacity = 0;
+        isModalRegOpen = 0
     }
 }
 
 const openModal = () => {
     modalWind.style.visibility = 'visible'
     modalWind.style.opacity = 1;
+    isModalLogOpen = 1
+    if (isModalRegOpen === 1) {
+         modalRegWind.style.visibility = 'visible'
+        modalRegWind.style.opacity = 1;
+    }
 }
 
 const openRegModal = () => {
      modalRegWind.style.visibility = 'visible'
     modalRegWind.style.opacity = 1;
-}
-const closeRegModal = event = {
-    
+    isModalRegOpen = 1;
+    if (isModalLogOpen === 1) {
+        modalWind.style.visibility = 'visible'
+    modalWind.style.opacity = 1;
+    }
 }
 
 buttonOpenReg.addEventListener("click", openRegModal)
@@ -53,3 +66,4 @@ buttonOpenLog.addEventListener("click", openModal)
 modalWind.addEventListener("click", closeModal)
 crossCloseReg.addEventListener("click", closeModal)
 modalRegWind.addEventListener("click",closeModal)
+headerClose.addEventListener("click", closeModal)
